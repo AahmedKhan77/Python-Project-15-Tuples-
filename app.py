5bc4abc187634a4b870bc43eed1a47f9839e55fc
Aahmed Khan <khanaahmed272@gmail.com>
2024-08-01 14:55:28 +0500

    Learned about tuples and unpacking

---
+++ app.py
@@ -0,0 +1,2 @@
+numbers = (1, 2, 3)#Tuples are immutable/unchangeable
+print(numbers[0])
---
+++ app2.py
@@ -0,0 +1,3 @@
+coordinates = (1, 2, 3)#can use on lists too aswell as tuples
+x, y, z = coordinates
+print(y)
